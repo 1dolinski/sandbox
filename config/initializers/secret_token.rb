9822282332ff33b935b45cc13440a89a2729b519
Christopher Dolinski <christopher.dolinski@gmail.com>
2012-08-14 00:11:37 -0400

    original setup; used git for mac for the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = '55bf60bb3ab3bd644a873e5d0764075a21706e2645516e705e0bb52e4999af66b807ca6022f3f94a539bafdde26865f70c6cb093f2e4318e3c167b5cd9b7cbb9'
